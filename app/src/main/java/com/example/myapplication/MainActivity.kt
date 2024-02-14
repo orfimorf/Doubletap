cfd1943ef476acd1c0c8ab476395e520881faa12
orfimorf <ilya.lapushkin@mail.ru>
2024-02-14 23:35:41 +0500

    del лишнего метода + логи в IDLE

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
@@ -14,35 +15,41 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
 
         findViewById<TextView>(R.id.textView).text = Storage.getNumber().toString()
 
         findViewById<Button>(R.id.button).setOnClickListener {
+            Log.i("MainActivity", "Переход на вторую страницу")
             Storage.switchChange()
             val sendIntent = Intent(this, SecondActivity::class.java).apply {  }
             startActivity(sendIntent)
-
-
         }
 
-
+        Log.i("MainActivity", "Отрисовка главной страницы")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.i("MainActivity", "Отрисовка главной страницы после закрытия второй")
         findViewById<TextView>(R.id.textView).text = Storage.getNumber().toString()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        if(Storage.getChange()) Storage.plusNumber()
+        if(Storage.getChange()) {
+            Log.i("MainActivity", "+1 к значению")
+            Storage.plusNumber()
+        }
+        Log.i("MainActivity", "Сохранение состояний в Bundle")
         outState.putInt("valueCounter", Storage.getNumber())
         super.onSaveInstanceState(outState)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
+        Log.i("MainActivity", "Востановление состояний из Bundle")
         Storage.setNumber(savedInstanceState.getInt("valueCounter"))
     }
 
--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import kotlin.math.pow
@@ -10,18 +11,17 @@ import kotlin.math.pow
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("SecondActivity", "Отрисовка второй страницы")
         setContentView(R.layout.activity_second)
         findViewById<TextView>(R.id.textView2).text =
             Storage.getNumber().toDouble().pow(2.toDouble()).toString()
 
         findViewById<Button>(R.id.backButton).setOnClickListener {
+            Log.i("SecondActivity", "Закрытие второй страницы")
             Storage.switchChange()
             this.finish()
         }
 
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-    }
 }
